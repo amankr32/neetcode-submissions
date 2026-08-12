c146717d4043285c9905eb255ef4ece195746a18
Aman Kumar <amankumar005562@gmail.com>
2026-08-13 00:46:51 +0530

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int mySqrt(int x) {
+        int start = 0;
+        int end = x;
+        int res = 0;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if ((long) mid*mid < x) {
+                start = mid + 1;
+                res = mid;
+            } else if ((long) mid*mid > x) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return res;
+    }
+}
